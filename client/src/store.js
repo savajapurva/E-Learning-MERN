fbb229fc8f07bf47ef5584da4fccc410eeb7733d
todoyalee <mohamedali.belkouri@smartqualitygate.com>
2023-04-21 21:01:01 +0100

    edit

--- client/src/store.js
+++ client/src/store.js
@@ -15,7 +15,7 @@ const store = createStore(
 
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
